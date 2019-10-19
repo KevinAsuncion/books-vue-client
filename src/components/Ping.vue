877c684012ffcb9a11e334525ad19023c43239b8
KevinAsuncion <kevinramosasuncion@gmail.com>
2019-10-19 12:20:32 -0700

    added routing, changed linting, removed boiler components, test request/response

---
+++ src/components/Ping.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>{{msg}}</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Ping",
+  data() {
+    return {
+      msg: ""
+    };
+  },
+  methods: {
+    async getMessage() {
+      try {
+        const path = "http://127.0.0.1:5000/ping";
+        const res = await axios.get(path);
+        this.msg = res.data;
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
+  },
+  created() {
+    this.getMessage();
+  }
+};
+</script>
+
+<style scoped>
+</style>
